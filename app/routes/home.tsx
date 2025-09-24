19867bccac6137e32471fa99441e6b9606505663
Ojasvi Rana <ojasviranaofficial@gmail.com>
2025-09-24 15:59:35 +0530

    first commit

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,19 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resumind" },
+    { name: "description", content: "Smart Feedback for your dream job!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+      <section className="main-section">
+          <div className="page-section">
+              <h1>Track your Applications & Resume</h1>
+              <h2>Review your submissions & AI-powered feedback!</h2>
+          </div>
+      </section>
+  </main>
 }
